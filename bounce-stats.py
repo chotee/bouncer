3f4611536322c17c9e5b68e2732ddb029b69d4b4
chotee <chotee@openended.eu>
2017-06-04 01:14:59 +0200

    Recombining switches into one longer switch when they are too close together to be separate events.

--- bounce-stats.py
+++ bounce-stats.py
@@ -28,8 +28,15 @@ class Switch():
         return len(self.transitions)
 
     @property
-    def switch_duration(self):
+    def duration(self):
         return self.transitions[-1][1]
+
+    def combine_next_switch(self, next_switch):
+        start_time_diff = next_switch.start_moment - self.start_moment
+        for transition in next_switch.transitions:
+            self.transitions.append([transition[0], transition[1]+start_time_diff])
+        self.end_state = next_switch.end_state
+
 
 def start_decoder(line, switches):
     parts = line.split(b":")
@@ -66,6 +73,26 @@ def line_reader(fd):
         yield line.strip()
 
 
+def combine_switches(switches, config):
+    prev_moment = 0
+    combined_switches = []
+    for switch in switches:
+        start_moment_difference = switch.start_moment - prev_moment
+        if start_moment_difference > config.settle_time or start_moment_difference < 0:
+            # print("Event started at:", start_moment_difference,
+            #       "transitions:", switch.transition_count,
+            #       "duration:", switch.duration)
+            combined_switches.append(switch)
+        else:
+            # print("Recombine with prev:", start_moment_difference)
+            prev_switch = combined_switches[-1]
+            prev_switch.combine_next_switch(switch)
+            # print("Event now has tranitions:", prev_switch.transition_count, "duration:", prev_switch.duration)
+
+        prev_moment = switch.start_moment + switch.duration
+    return combined_switches
+
+
 def create_stats(switches):
     switch_count = len(switches)
     print("Total recorded switches: %d" % switch_count)
@@ -74,20 +101,22 @@ def create_stats(switches):
     avg_transitions  = sum([s.transition_count for s in switches]) / switch_count
     print("Most/Least/avg transitions for a switch", most_transitions, least_transitions, avg_transitions)
 
-    slowest_switch  = max([s.switch_duration for s in switches])
-    quickest_switch = min([s.switch_duration for s in switches])
-    avg_switch = sum([s.switch_duration for s in switches]) / switch_count
+    slowest_switch  = max([s.duration for s in switches])
+    quickest_switch = min([s.duration for s in switches])
+    avg_switch = sum([s.duration for s in switches]) / switch_count
     print("Slowest/Quickest/avg switch", slowest_switch, quickest_switch, avg_switch)
 
 
 def main(argv):
     parser = argparse.ArgumentParser(description="Bounce data to statistics converter")
     parser.add_argument('datafiles', metavar="FILE", nargs=1, help="The bounce datafile")
+    parser.add_argument('--settle-time', metavar='µsecs', help='number of microseconds between switches. Will recombine switches that are shorter then this', default=200000)
     config = parser.parse_args(argv[1:])
 
     for datafile in config.datafiles:
         with open(datafile, 'rb') as fd:
             switches = parse_file(fd, config)
+            switches = combine_switches(switches, config)
             create_stats(switches)
 if __name__ == '__main__':
     main(sys.argv)
